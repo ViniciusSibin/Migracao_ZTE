34106bc8c56b51dd84b3b6ecdb2dd2c5cddc6970
Jbampa <jbampaa@gmail.com>
2023-10-03 15:00:24 -0300

    voltando a merda que eu fiz

--- Pages/gerenciar.php
+++ Pages/gerenciar.php
@@ -5,28 +5,45 @@
 </head>
 <body>
     <h1>Gerenciar Arquivos</h1>
+    <a href="../index.php">Voltar para o sistema</a>
     <?php
+    // Caminho para a pasta "Arquivos"
     $caminhoArquivos = '../Arquivos';
-    $pastaAtual = isset($_GET['pasta']) ? $caminhoArquivos . '/' . $_GET['pasta'] : $caminhoArquivos;
-    $arquivos = scandir($pastaAtual);
 
-    if ($pastaAtual !== $caminhoArquivos) {
-        echo '<a href="gerenciar.php?pasta=' . urlencode(dirname($pastaAtual)) . '">Voltar para a pasta anterior</a><br>';
-        echo '<a href="../index.php">Voltar para o sistema</a><br>';
+    // Verifique se o parâmetro "pasta" está definido na URL
+    if (isset($_GET['pasta'])) {
+        $pastaAtual = $caminhoArquivos . '/' . $_GET['pasta'];
     } else {
-        echo '<a href="../index.php">Voltar para o sistema</a><br>';
+        $pastaAtual = $caminhoArquivos;
     }
-
+ 
+    // Lista os arquivos e subpastas na pasta atual
+    $arquivos = scandir($pastaAtual);
     foreach ($arquivos as $arquivo) {
         if ($arquivo != '.' && $arquivo != '..') {
             $caminhoCompleto = $pastaAtual . '/' . $arquivo;
+
             if (is_dir($caminhoCompleto)) {
-                echo '<p><strong>Pasta: </strong><a href="gerenciar.php?pasta=' . urlencode($caminhoCompleto) . '">' . $arquivo . '</a></p>';
+                // Verifica se a pasta atual é uma subpasta de "Arquivos"
+                if ($pastaAtual !== $caminhoArquivos) {
+                    $pastaAnterior = dirname($pastaAtual);
+                    $nomePastaAnterior = basename($pastaAnterior);
+                    
+                    if ($nomePastaAnterior === "Arquivos") {
+                        echo '<p><strong>Pasta: </strong><a href="gerenciar.php?pasta=' . urlencode($caminhoCompleto) . '">' . $arquivo . '</a> - <a href="excluir.php?dir=' . urlencode($caminhoCompleto) . '">Excluir</a></p>';
+                    }
+                }else {
+                    // Se a pasta for uma subpasta de "Arquivos", exibir a opção de exclusão da subpasta
+                    echo '<p><strong>Pasta: </strong><a href="gerenciar.php?pasta=' . urlencode($caminhoCompleto) . '">' . $arquivo . '</a></p>';
+                }
             } elseif (pathinfo($arquivo, PATHINFO_EXTENSION) == 'txt') {
+                // Exibir link para visualizar o arquivo
                 echo '<p><strong>Arquivo: </strong>' . $arquivo . ' - <a href="' . $caminhoCompleto . '" target="_blank">Visualizar</a></p>';
             }
         }
     }
+
     ?>
+
 </body>
 </html>
